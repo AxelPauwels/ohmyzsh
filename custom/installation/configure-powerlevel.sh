83bc47d3538f86f2d017d8481b3fd10076b1ac01
Axel Pauwels <axel.pauwels@MacBook-Air.local>
2026-08-25 09:40:23 +0200

    Updating p10k configure script to change username/nickname

--- custom/installation/configure-powerlevel.sh
+++ custom/installation/configure-powerlevel.sh
@@ -23,6 +23,7 @@ showConfigureMessage() {
     msg_title "How do you want to configure?"
     msg "(1) Use the Powerlevel10k configuration wizard (recommended)"
     msg "(2) Reset to saved configuration in git"
+    msg "(3) Change Powerlevel10k username/nickname"
     msg_dimmed "(q) Quit"
     ConfigureMessageIsShown=true
   fi
@@ -48,12 +49,41 @@ while true; do
     exec zsh
     break
     ;;
+  3)
+    p10k_config="$HOME/.p10k.zsh"
+    if [ ! -f "$p10k_config" ]; then
+      cp "$ZSH_INSTALL"/resources/themes/.p10k.zsh "$p10k_config"
+    fi
+
+    read -r -p "Enter username/nickname: " p10k_nickname
+    temp_p10k_config=$(mktemp)
+    if awk -v nickname="$p10k_nickname" '
+      /^[[:space:]]*#/ { print; next }
+      /^[[:space:]]*POWERLEVEL9K_CONTEXT_TEMPLATE=/ {
+        print "POWERLEVEL9K_CONTEXT_TEMPLATE='\''" nickname "'\''"
+        replaced=1
+        next
+      }
+      { print }
+      END { exit(replaced ? 0 : 1) }
+    ' "$p10k_config" >"$temp_p10k_config"; then
+      mv "$temp_p10k_config" "$p10k_config"
+      msg_success "Saved Powerlevel10k username/nickname."
+    else
+      rm -f "$temp_p10k_config"
+      msg_error "Could not find an uncommented POWERLEVEL9K_CONTEXT_TEMPLATE= line in $p10k_config."
+    fi
+
+    new_line
+    ConfigureMessageIsShown=false
+    ;;
   q | Q)
-    exit
+    exec zsh
+    break
     ;;
   *)
     new_line
-    msg_italic "Please choose a option (1/2/q)"
+    msg_italic "Please choose an option (1/2/3/q)"
     ;;
   esac
 done
